704e8c9f00393a302d9fcaf8ddb997783c89bb22
BBUSRA <busratur1@gmail.com>
2020-08-19 12:55:06 +0300

    add catalog

--- catalog/catalog/settings.py
+++ catalog/catalog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig', #kendi oluşturduğumuz dosyayı projenin bir parçası yaptık
+    'movies',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
